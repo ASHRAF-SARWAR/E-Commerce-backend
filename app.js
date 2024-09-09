4c9d07cd255c493ff53d33875a1766337c8df630
RMWajahat <rajawajahat2003@gmail.com>
2024-09-09 21:49:15 +0500

    vercel and deployment settings

--- app.js
+++ app.js
@@ -27,12 +27,4 @@ app.use("/cart", cartRouter);
 app.use("/admins", adminRouter);
 app.use("/orders", orderRouter);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, (error) => {
-  if (error) {
-    console.log("Error: ", error);
-  } else {
-    console.log("Server is running on port 3000");
-  }
-});
+export default app;
